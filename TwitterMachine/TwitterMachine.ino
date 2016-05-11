e8526e508f97619c7ad4ba71a244178e7caa3811
Marco Loiodice <mloiodice13@gmail.com>
2016-05-11 14:46:48 +0200

    Send button function added

--- TwitterMachine/TwitterMachine.ino
+++ TwitterMachine/TwitterMachine.ino
@@ -22,6 +22,9 @@ char *MyTweet;
 char Hashtag[]= "#With1Sheeld";
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int inputButtonSend = 8;
+int inputButtonCancel = 9;
+int valSend = 0;
 
 
 void setup()
@@ -31,31 +34,36 @@ void setup()
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("Tweet Machine !");
+  pinMode(inputButtonSend, INPUT);
   
 }
 
 void loop()
 {
+  valSend = digitalRead(inputButtonSend);  // read input value
+       
   if(VoiceRecognition.isNewCommandReceived()){
-
     MyTweet = VoiceRecognition.getLastCommand();
-
+    lcd.clear();
     lcd.setCursor(0,0);
     lcd.print(MyTweet);
     lcd.setCursor(0, 1);
     // print the number of seconds since reset:
     lcd.print(Hashtag);
+  }
 
+  int lengthTweet = strlen(MyTweet);
+  
+  if(valSend != 0 && lengthTweet != 0){
     strcat(MyTweet, " ");
     strcat(MyTweet, Hashtag);
-    int lengthTweet = strlen(MyTweet);
-
-    if(lengthTweet != 0){
-      Twitter.tweet(MyTweet);
+    Twitter.tweet(MyTweet);
       TextToSpeech.say("Tweet send !");
       Notification.notifyPhone("Tweet envoyé");
+      lcd.setCursor(0, 1);
+      // print the number of seconds since reset:
+      lcd.print("Tweet send !");
       strcpy(MyTweet, "");
-    } 
   }
 }
